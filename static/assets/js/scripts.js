1362ce71f3bf3e84daab43e814ce6b9df061464a
Flaubert SEO <flaubertweb@gmail.com>
2025-06-22 16:25:01 -0300

    Projeto com carrocel e opção de selecionar personagem

---
+++ static/assets/js/scripts.js
@@ -0,0 +1,37 @@
+  
+  
+  /*PARALAX FUNDO*/
+  const parallax = document.getElementById('parallax');
+  document.addEventListener('mousemove', (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 10; // -5 a +5
+    const y = (e.clientY / window.innerHeight - 0.5) * 10;
+
+    parallax.style.backgroundPosition = `${50 + x}% ${50 + y}%`;
+  });
+
+
+
+/*LETREIRO ANIMADO */
+  document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('.animate').forEach(container => {
+    const spans = container.querySelectorAll('span');
+    spans.forEach((span, i) => {
+      span.style.animationDelay = `${i * 0.05}s`;
+    });
+  });
+});
+
+
+ 
+  function selecionarPersonagem(botao) {
+    // Remove a classe "selecionado" de todos os cards
+    document.querySelectorAll('.finish-card.selecionado').forEach(card => {
+      card.classList.remove('selecionado');
+    });
+
+    // Adiciona "selecionado" apenas no card pai do botão clicado
+    const card = botao.closest('.finish-card');
+    if (card) {
+      card.classList.add('selecionado');
+    }
+  }
